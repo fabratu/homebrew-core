e60c85d39a36d611bc46e35b19e1e4acfa81e6bf
Fabian Brandt-Tumescheit <fabian.brandt-tumescheit@informatik.hu-berlin.de>
2020-09-10 17:18:20 +0200

    Updated package NetworKit and all dependencies.

--- Formula/networkit.rb
+++ Formula/networkit.rb
@@ -4,18 +4,9 @@ class Networkit < Formula
   desc "Performance toolkit for large-scale network analysis"
   homepage "https://networkit.github.io"
   url "https://github.com/networkit/networkit.git",
-      tag:      "7.1",
-      revision: "4c6dcc4367b51005a34221242048609c357ffbd6"
-  license "MIT"
-
-  bottle do
-    sha256 "9ad19c9b34c80145ae33780156942bb83a092d7bc7c91701c75ae38d849fd867" => :catalina
-    sha256 "623478b0de8335e4547ba153e1c897276928c491c9150af1725941e758218906" => :mojave
-    sha256 "478b25c8e7df43389a444ab4368ce457912001d38501f1f4d248b96a10d24552" => :high_sierra
-  end
+      tag: "7.1", revision: "4c6dcc4367b51005a34221242048609c357ffbd6"
 
   depends_on "cmake" => :build
-  depends_on "cython" => :build
   depends_on "libnetworkit"
   depends_on "numpy"
   depends_on "python@3.8"
@@ -25,15 +16,13 @@ class Networkit < Formula
     xy = Language::Python.major_minor_version Formula["python@3.8"].opt_bin/"python3"
     rpath_addons = Formula["libnetworkit"].opt_lib
 
-    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python#{xy}/site-packages/"
-    ENV.append_path "PYTHONPATH", Formula["cython"].opt_libexec/"lib/python#{xy}/site-packages"
-    system Formula["python@3.8"].opt_bin/"python3", "setup.py", "build_ext",
-          "--networkit-external-core",
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+
+    system Formula["python@3.8"].opt_bin/"pip3", "install", "cython"
+    system Formula["python@3.8"].opt_bin/"python3", "setup.py", "build_ext", "--networkit-external-core",
            "--rpath=@loader_path;#{rpath_addons}"
-    system Formula["python@3.8"].opt_bin/"python3", "setup.py", "install",
-           "--single-version-externally-managed",
-           "--record=installed.txt",
-           "--prefix=#{libexec}"
+    system Formula["python@3.8"].opt_bin/"python3", "setup.py", "install", "--single-version-externally-managed",
+           "--record=installed.txt", "--prefix=#{libexec}"
     site_packages = "lib/python#{xy}/site-packages"
     pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
     (prefix/site_packages/"homebrew-networkit.pth").write pth_contents
